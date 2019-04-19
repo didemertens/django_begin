6347fb2e426cb496195a9385b33edfc65e155544
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-19 17:44:11 +0100

    added bootstrap and crispy forms

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+
+    'crispy_forms',
+
+    # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
 ]
@@ -125,3 +131,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
