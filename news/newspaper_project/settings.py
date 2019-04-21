05f8b38bb05d2610f9c21dc407d7b946f62b8ad2
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-21 08:38:21 +0100

    added comments, article edit/delete

--- news/newspaper_project/settings.py
+++ news/newspaper_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     # local
     'users.apps.UsersConfig',
     'pages.apps.PagesConfig',
+    'articles.apps.ArticlesConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
